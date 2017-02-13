fde3e146c99251c3a195e16f061b945b93b1e4e7
Pavel <pavelsobchenko@gmail.com>
2017-02-13 19:37:57 +0200

    optimization mapping

--- js/Constants.js
+++ js/Constants.js
@@ -13,6 +13,131 @@ var app = app || {};
         STROKE_STYLE: '#3b6cba',
         FILL_STYLE: '#1464e5'
     };
+    var r = app.const.CELL, c = (app.const.WIDTH/2)-(r/2);
 
-    app.const.ROW_WIDTH = app.const.WIDTH/app.const.CELL;
+    // count cells in one row
+    app.const.ROW_WIDTH = app.const.WIDTH/r;
+
+    // figures
+    app.const.FIGURE = [
+        [
+            {x:(c-(r*2)), y:-r, left: true},
+            {x:(c-r), y:-r},
+            {x:c, y:-r, center: true},
+            {x:(c+r), y:-r, right: true}
+        ], // row
+        [
+            {x:(c-r), y:-(r*2), left: true},
+            {x:c, y:-(r*2), right: true},
+            {x:(c-r), y:-r, left: true},
+            {x:c, y:-r, right:true}
+        ], // rect
+        [
+            {x:c, y:-(r*3), left: true, right: true},
+            {x:c, y:-(r*2), left: true, right: true, center: true},
+            {x:c, y:-r, left: true},
+            {x:(c+r), y:-r, right: true}
+        ], // L
+        [
+            {x:c, y:-(r*3), left: true, right: true},
+            {x:c, y:-(r*2), left: true, right: true, center: true},
+            {x:c, y:-r, right: true},
+            {x:c-r, y:-r, left: true}
+        ], // RL
+        [
+            {x:c,y:0-r, right: true, left: true},
+            {x:(c-r),y:-(r*2) , left: true},
+            {x:c,y:0-(r*2), center: true},
+            {x:(c+r),y:-(r*2), right: true}
+        ], // T
+        [
+            {x:c-r, y: 0-(r*2), left: true},
+            {x:c, y: 0-(r*2), right: true, center: true},
+            {x:c, y: 0-r, left: true},
+            {x:c+r, y: 0-r, right: true}
+        ], // Z
+        [
+            {x: c+r, y: 0-(r*2), right: true},
+            {x: c, y: 0-(r*2), center: true, left: true},
+            {x: c, y: 0-r, right: true},
+            {x: c-r, y: 0-r, left: true}
+        ] // RZ
+    ];
+    app.const.TYPE_MAP = ['I', 'C', 'L', 'RL', 'T', 'Z', 'RZ'];
+    app.const.TRANSFORM_MAP = {
+        'I': [
+            [[2, -2], [1, -1], null, [-1, 1]],
+            [[-2, 2], [-1, 1], null, [1, -1]],
+            [[2, -2], [1, -1], null, [-1, 1]],
+            [[-2, 2], [-1, 1], null, [1, -1]]
+        ],
+        'L': [
+            [[1, 1], null, [-1, -1], [-2, 0]],
+            [[-1, 1], null, [1, -1], [0, -2]],
+            [[-1, -1], null, [1, 1], [2, 0]],
+            [[1, -1], null, [-1, 1], [0, 2]]
+        ],
+        'RL': [
+            [[1, 1], null, [-1, -1], [0, -2]],
+            [[-1, 1], null, [1, -1], [2, 0]],
+            [[-1, -1], null, [1, 1], [0, 2]],
+            [[1, -1], null, [-1, 1], [-2, 0]]
+        ],
+        'T': [
+            ['1', [1, -1], null, '0'],
+            ['1', [1, 1], null, '0'],
+            ['1', [-1, 1], null, '0'],
+            ['1', [-1, -1], null, '0']
+        ],
+        'Z': [
+            [[1, -1], null, '0', [-2, 0]],
+            [[-1, 1], null, [1, 1], [2, 0]],
+            [[1, -1], null, '0', [-2, 0]],
+            [[-1, 1], null, [1, 1], [2, 0]]
+        ],
+        'RZ': [
+            ['2', null, [-1, -1], [0, -2]],
+            [[1, -1], null, '0', [0, 2]],
+            ['2', null, [-1, -1], [0, -2]],
+            [[1, -1], null, '0', [0, 2]]
+        ]
+    };
+    app.const.POSITION_MAP = {
+        'I': [
+            [['l', 'r'], ['l', 'r'], ['l', 'r', 'c'], ['l', 'r']],
+            ['l', null, 'c', 'r'],
+            [['l', 'r'], ['l', 'r'], ['l', 'r', 'c'], ['l', 'r']],
+            ['l', null, 'c', 'r']
+        ],
+        'L': [
+            ['r', 'c', 'l', ['l', 'r']],
+            [['l', 'r'], ['l', 'r', 'c'], 'r', 'l'],
+            ['l', 'c', 'r', ['l', 'r']],
+            [['l', 'r'], ['l', 'r', 'c'], 'l', 'r']
+        ],
+        'RL': [
+            ['r', 'c', 'l', ['l', 'r']],
+            [['l', 'r'], ['c', 'l', 'r'], 'l', 'r'],
+            ['l', 'c', 'r', ['l', 'r']],
+            [['l', 'r'], ['c', 'l', 'r'], 'l', 'r']
+        ],
+        'T': [
+            ['l', ['l', 'r'], ['c', 'r'], ['l', 'r']],
+            [['l', 'r'], 'r', 'c', 'l'],
+            ['r', ['l', 'r'], ['l', 'c'], ['l', 'r']],
+            [['l', 'r'], 'l', 'c', 'r']
+        ],
+        'Z': [
+            [['l', 'r'], ['c', 'r'], 'l', ['l', 'r']],
+            ['l', ['c', 'r'], 'l', 'r'],
+            [['l', 'r'], ['c', 'r'], 'l', ['l', 'r']],
+            ['l', ['c', 'r'], 'l', 'r']
+        ],
+        'RZ': [
+            [['l', 'r'], ['c', 'r'], 'l', ['l', 'r']],
+            ['r', ['c', 'l'], 'r', 'l'],
+            [['l', 'r'], ['c', 'r'], 'l', ['l', 'r']],
+            ['r', ['c', 'l'], 'r', 'l']
+        ]
+    };
 })();
--- js/collections/FigureCollection.js
+++ js/collections/FigureCollection.js
@@ -10,54 +10,8 @@ var app = app || {};
         createFigure: function (intg) {
             var r = app.const.CELL, c = (app.const.WIDTH/2)-(r/2);
             this.currentState = 0;
-            var figure = [
-                [
-                    {x:(c-(r*2)), y:-r, left: true},
-                    {x:(c-r), y:-r},
-                    {x:c, y:-r, center: true},
-                    {x:(c+r), y:-r, right: true}
-                ], // row
-                [
-                    {x:(c-r), y:-(r*2), left: true},
-                    {x:c, y:-(r*2), right: true},
-                    {x:(c-r), y:-r, left: true},
-                    {x:c, y:-r, right:true}
-                ], // rect
-                [
-                    {x:c, y:-(r*3), left: true, right: true},
-                    {x:c, y:-(r*2), left: true, right: true, center: true},
-                    {x:c, y:-r, left: true},
-                    {x:(c+r), y:-r, right: true}
-                ], // L
-                [
-                    {x:c, y:-(r*3), left: true, right: true},
-                    {x:c, y:-(r*2), left: true, right: true, center: true},
-                    {x:c, y:-r, right: true},
-                    {x:c-r, y:-r, left: true}
-                ], // RL
-                [
-                    {x:c,y:0-r, right: true, left: true},
-                    {x:(c-r),y:-(r*2) , left: true},
-                    {x:c,y:0-(r*2), center: true},
-                    {x:(c+r),y:-(r*2), right: true}
-                ], // T
-                [
-                    {x:c-r, y: 0-(r*2), left: true},
-                    {x:c, y: 0-(r*2), right: true, center: true},
-                    {x:c, y: 0-r, left: true},
-                    {x:c+r, y: 0-r, right: true}
-                ], // Z
-                [
-                    {x: c+r, y: 0-(r*2), right: true},
-                    {x: c, y: 0-(r*2), center: true, left: true},
-                    {x: c, y: 0-r, right: true},
-                    {x: c-r, y: 0-r, left: true}
-                ] // RZ
-            ];
-            var type_map = ['I', 'C', 'L', 'RL', 'T', 'Z', 'RZ'];
-
-            this.type = type_map[intg];
-            this.reset(figure[intg]);
+            this.type = app.const.TYPE_MAP[intg];
+            this.reset(app.const.FIGURE[intg]);
         },
         rotateFigure: function () {
 
@@ -66,86 +20,13 @@ var app = app || {};
             if(this.currentState > 3)
                 this.currentState = 0;
 
-            var transform_map = {
-                'I': [
-                    [[2, -2], [1, -1], null, [-1, 1]],
-                    [[-2, 2], [-1, 1], null, [1, -1]],
-                    [[2, -2], [1, -1], null, [-1, 1]],
-                    [[-2, 2], [-1, 1], null, [1, -1]]
-                ],
-                'L': [
-                    [[1, 1], null, [-1, -1], [-2, 0]],
-                    [[-1, 1], null, [1, -1], [0, -2]],
-                    [[-1, -1], null, [1, 1], [2, 0]],
-                    [[1, -1], null, [-1, 1], [0, 2]]
-                ],
-                'RL': [
-                    [[1, 1], null, [-1, -1], [0, -2]],
-                    [[-1, 1], null, [1, -1], [2, 0]],
-                    [[-1, -1], null, [1, 1], [0, 2]],
-                    [[1, -1], null, [-1, 1], [-2, 0]]
-                ],
-                'T': [
-                    ['1', [1, -1], null, '0'],
-                    ['1', [1, 1], null, '0'],
-                    ['1', [-1, 1], null, '0'],
-                    ['1', [-1, -1], null, '0']
-                ],
-                'Z': [
-                    [[1, -1], null, '0', [-2, 0]],
-                    [[-1, 1], null, [1, 1], [2, 0]],
-                    [[1, -1], null, '0', [-2, 0]],
-                    [[-1, 1], null, [1, 1], [2, 0]]
-                ],
-                'RZ': [
-                    ['2', null, [-1, -1], [0, -2]],
-                    [[1, -1], null, '0', [0, 2]],
-                    ['2', null, [-1, -1], [0, -2]],
-                    [[1, -1], null, '0', [0, 2]]
-                ]
+            var data = app.const.TRANSFORM_MAP[this.type][this.currentState];
+            var position = app.const.POSITION_MAP[this.type][this.currentState];
+            var map = {
+                'l': 'left',
+                'r': 'right',
+                'c': 'center'
             };
-
-            var position_map = {
-                'I': [
-                    [['left', 'right'], ['left', 'right'], ['left', 'right', 'center'], ['left', 'right']],
-                    [['left'], null, ['center'], ['right']],
-                    [['left', 'right'], ['left', 'right'], ['left', 'right', 'center'], ['left', 'right']],
-                    [['left'], null, ['center'], ['right']]
-                ],
-                'L': [
-                    [['right'], ['center'], ['left'], ['left', 'right']],
-                    [['left', 'right'], ['left', 'right', 'center'], ['right'], ['left']],
-                    [['left'], ['center'], ['right'], ['left', 'right']],
-                    [['left', 'right'], ['left', 'right', 'center'], ['left'], ['right']]
-                ],
-                'RL': [
-                    [['right'], ['center'], ['left'], ['left', 'right']],
-                    [['left', 'right'], ['center', 'left', 'right'], ['left'], ['right']],
-                    [['left'], ['center'], ['right'], ['left', 'right']],
-                    [['left', 'right'], ['center', 'left', 'right'], ['right'], ['left']]
-                ],
-                'T': [
-                    [['left'], ['left', 'right'], ['center', 'right'], ['left', 'right']],
-                    [['left', 'right'], ['right'], ['center'], ['left']],
-                    [['right'], ['left', 'right'], ['left', 'center'], ['left', 'right']],
-                    [['left', 'right'], ['left'], ['center'], ['right']]
-                ],
-                'Z': [
-                    [['left', 'right'], ['center', 'right'], ['left'], ['left', 'right']],
-                    [['left'], ['center', 'right'], ['left'], ['right']],
-                    [['left', 'right'], ['center', 'right'], ['left'], ['left', 'right']],
-                    [['left'], ['center', 'right'], ['left'], ['right']]
-                ],
-                'RZ': [
-                    [['left', 'right'], ['center', 'right'], ['left'], ['left', 'right']],
-                    [['right'], ['center', 'left'], ['right'], ['left']],
-                    [['left', 'right'], ['center', 'right'], ['left'], ['left', 'right']],
-                    [['right'], ['center', 'left'], ['right'], ['left']]
-                ]
-            };
-
-            var data = transform_map[this.type][this.currentState];
-            var position = position_map[this.type][this.currentState];
             var tmp = [];
             var r = app.const.CELL;
 
@@ -166,9 +47,13 @@ var app = app || {};
                 }
 
                 if(position[i]) {
-                    _.each(position[i], function (name) {
-                       item[name] = true;
-                    });
+                    if(_.isString(position[i])) {
+                        item[map[position[i]]] = true;
+                    } else {
+                        _.each(position[i], function (name) {
+                            item[map[name]] = true;
+                        });
+                    }
                 }
                 tmp.push(item);
             }.bind(this));
